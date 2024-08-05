fffe8467d3d1a7b74d1a7dac3e0e6460fd3d01a1
Rohith Gajawada <141448174+RohithGajawada45@users.noreply.github.com>
2024-08-05 14:42:59 +0530

    Update useChat.jsx

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = "http://localhost:3000";
+const backendUrl = "https://virtual-friend-backend.vercel.app/";
 
 const ChatContext = createContext();
 
